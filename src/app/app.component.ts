c29b0f374213f13c8d3b2a07ed6deb52dc5d15d6
Toung-test <C108193246@nkust.edu.tw>
2025-12-09 10:03:34 +0800

    1141209介紹 IF 跟 switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,108 +10,33 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 
-  newArray: Array<string> = ['one', 'two'];
+  displayH1: boolean = false;
 
-  arrayDataTwo = [
-    {
-      id: 1,
-      name: 'a',
-      userData: [
-        { userID: 1 }
-      ]
-    },
-    {
-      id: 2,
-      name: 'b',
-      userData: [
-        { userID: 2 }
-      ]
-    },
-    {
-      id: 3,
-      name: 'c',
-      userData: [
-        { userID: 3 }
-      ]
-    },
-  ];
-
-  userArray = [
-    {
-      userName: "玩家 A",
-      lev: 10,
-      props: [
-        {
-          propsName: '蘑菇',
-          amount: 5
-        },
-        {
-          propsName: '金幣',
-          amount: 15
-        }
-      ]
-    },
-    {
-      userName: "玩家 B",
-      lev: 15,
-      props: [
-        {
-          propsName: '龜殼',
-          amount: 1
-        },
-        {
-          propsName: '砲彈',
-          amount: 15
-        }
-      ]
+  changeDisplayH1() {
+    if (this.displayH1) {
+      this.displayH1 = false;
+    } else {
+      this.displayH1 = true;
     }
-  ]
 
+  }
   // ngOnInit 是網頁的生命週期，在網頁一開始的時候就會被觸發
   ngOnInit(): void {
-    let jsonData = {
-      name: "Allen",
-      age: 18,
-      favorite: {
-        commodity: [
-          "商品1",
-          "商品2",
-          "商品3"
-        ]
-      },
-      memberData: [
-        {
-          money: 5000,
-          email: "abc@gmail.com"
-        }
-      ]
+    let num = 5;
+    switch (num) {
+      case 1:
+        console.log(1);
+        break;
+      case 2:
+        console.log(2);
+        break;
+      case 5:
+        console.log(5);
+        break;
+      default:
+        console.log('default');
+        break;
     }
 
-    // 死板的寫法
-    let newData = {
-      userName: jsonData.name,
-      userAge: jsonData.age,
-      likefavorite: jsonData.favorite,
-      memberData: [
-        {
-          payMoney: jsonData.memberData[0].money,
-          email: jsonData.memberData[0].email
-        }
-      ]
-    };
-
-    let newDataTwo = {
-      userName: jsonData.name,
-      userAge: jsonData.age,
-      likefavorite: [...jsonData.favorite.commodity],
-      memberData: [jsonData.memberData.map(item => ({
-        payMoney: item.money,
-        email: item.email
-      }))
-      ]
-    }
-
-    console.log(newData);
-    console.log(newDataTwo);
   }
 }
