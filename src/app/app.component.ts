fa8c21b20b0199ad74248475afc9b6ac092fe316
Toung-test <C108193246@nkust.edu.tw>
2025-12-16 13:53:14 +0800

    介紹tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
-// 以下這個自動引入是錯誤的
-// import { NgClass } from "../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgClass],
+  imports: [RouterOutlet, MatTabsModule, MatButtonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 
 export class AppComponent {
-  showClass: boolean = false;
+  links = [
+    { name: 'First', path: '/first' },
+    { name: 'Second', path: '/second' },
+    { name: 'Third', path: '/third' }
+  ];
+  activeLink = this.links[0].path;
 
-  showH4Css(): string {
-    return "h4Css";
-  }
+
+  // 原先的呈現純切換標籤
+  // links = ['First', 'Second', 'Third'];
+  // activeLink = this.links[0];
+
+  // 新增頁面的，這次不會用到
+  // addLink() {
+  //   this.links.push(`Link ${this.links.length + 1}`);
+  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { NewComponent } from './about/new/new.component';
+import { ShopComponent } from './shop/shop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'first', component: AboutComponent },
+  { path: 'second', component: NewComponent },
+  { path: 'third', component: ShopComponent },
+];
